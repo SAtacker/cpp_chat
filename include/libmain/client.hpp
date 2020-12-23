dc7962513568da74ba475a34e63b7bccf40ce4aa
Shreyas <shreyasatre16@gmail.com>
2020-12-23 19:08:24 +0530

    Renamed hello_world.hpp to client.hpp

---
+++ include/libmain/client.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <boost/asio.hpp>
+
+namespace libmain {
+    class client{
+        private:
+            std::string input_message;
+            std::string incomming_message;
+            std::string client_name;
+            std::string address;
+            int port;
+            boost::asio::io_service io_service;
+            boost::asio::ip::tcp client_socket;
+
+        public:
+            client();
+            ~client();
+            void get_inp_message();
+            void send_message();
+            void receive_message();
+    };
+}
