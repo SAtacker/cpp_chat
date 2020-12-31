a93a5b6d1dd8ac660aa6fc4eafef86b94b55f932
Shreyas <shreyasatre16@gmail.com>
2020-12-31 17:27:23 +0530

    Fixed Minor Bugs

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,27 +1,27 @@
-#include "libmain/chat.hpp"
+#include "libmain/client.hpp"
+#include "libmain/server.hpp"
 
 using namespace std;
 using namespace libmain;
-// using namespace boost;
 
 void handle_client(){
-    chat cl;
-    cl.get_inp_message();
-    cl.send_message();
+    client cl{"127.0.0.1",5100,"Human"};
+    while(true){
+        cl.get_inp_message();
+        cl.send_message();
+    }
 }
 
-void display_client(){
-    chat cl{"127.0.0.1",50000,"Shreyas"};
-    cl.receive_message();
-    cl.display_received();
+void serrver_echo(){
+    server s{"127.0.0.1",5100};
+    s.init_server();
 }
 
 int main() {
-    // boost::thread  c{handle_client},t{display_client};
-    // if(c.joinable())
-    // c.join();
-    // if(t.joinable())
-    // t.join();
-    handle_client();
+    boost::thread c{handle_client},t{serrver_echo};
+    if(c.joinable())
+    c.join();
+    if(t.joinable())
+    t.join();
     return 0;
 }
