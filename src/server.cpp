4277773ab484bf3f7992c9345f96582fdea052a9
Shreyas <shreyasatre16@gmail.com>
2021-01-06 17:06:28 +0530

    Added main handler functions

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,19 @@
 #include "libchat/server.hpp"
 
-libserver::server::server(boost::asio::io_service service) : socket(service)
+std::vector<std::string> libserver::server::cl_list;
+
+
+/*
+    Regarding shared from this https://stackoverflow.com/questions/3629557/boost-shared-from-this
+
+    When a single connection ends, all associated asynchronous operations complete. 
+    The corresponding handler objects are destroyed, and all shared_ptr references to the objects are destroyed.
+    The io_service destructor destroys all handlers, causing all shared_ptr references to all connection objects to be destroyed.
+
+*/
+
+
+libserver::server::server() : socket(libserver::io_service)
 {
     _is_reading = false;
     _num_bytes_read = 0;
@@ -70,8 +83,10 @@ void libserver::server::_process_request_()
         _num_bytes_read -= (std::find(_incomming_message,_incomming_message+_num_bytes_read,'\n')-_incomming_message) + 1;
 
         if(msg.find(_LOGIN_REQUEST_)==0)
-        {
+        {   
+            client_name = msg.substr(sizeof(_LOGIN_REQUEST_));
             _handle_login_req();
+            libserver::_update_client_changed();
         }
         else if(msg.find(_PING_REQUEST_)==0)
         {
@@ -83,33 +98,85 @@ void libserver::server::_process_request_()
         }
         else if(msg.find(_REQ_ECHO_)==0)
         {
-            _display_message();
+            std::string m;
+            m = msg.substr(sizeof(_REQ_ECHO_));
+            _display_message(m);
         }
     }
 }
 
 void libserver::server::_handle_login_req()
 {
-    // Handle usning unordered map
+    // TODO::Handle usning unordered map
+    std::cout<<client_name<<" logged in"<<std::endl;
+    _write_to_socket(_LOGIN_SUCCESS_+"\n");
+    _set_map_change();
 }
 
 void libserver::server::_handle_ping_req()
 {
     // Respond to ping request
+    _write_to_socket(_map_change? "ping list changed\n":(_PING_RESPONSE_+"\n"));
+    _map_change = false;
 }
 
 void libserver::server::_handle_list_request()
 {
     // Respond with Usernames of people currently
+    std::string unames;
+    for(auto it:cl_list)unames+=it;
+    unames+="\n";
+    _write_to_socket(unames);
+
 }
 
-void libserver::server::_display_message()
+void libserver::server::_display_message(const std::string& _display_msg)
 {
     // Display a nice formatted message
+    std::cout<<client_name<<" : "<<_display_message<<std::endl;
 }
 
 
 void libserver::server::_write_to_socket(const std::string& _send_message)
 {
     socket.write_some(boost::asio::buffer(_send_message));
+}
+
+const std::string libserver::server::_get_username()
+{
+    return client_name;
+}
+
+boost::asio::ip::tcp::socket& libserver::server::_get_socket() 
+{
+    return socket; 
+}
+
+void libserver::_update_client_changed()
+{
+    for( libserver::server_ptr_vector::iterator b = libserver::server_shared_ptrs.begin(), e = libserver::server_shared_ptrs.end(); b != e; ++b)
+        (*b)->_set_map_change();
+}
+
+void libserver::multiple_client_handler()
+{
+    while ( true) {
+        boost::this_thread::sleep( boost::posix_time::millisec(1));
+        for ( libserver::server_ptr_vector::iterator b = libserver::server_shared_ptrs.begin(), e = libserver::server_shared_ptrs.end(); b != e; ++b) 
+            (*b)->answer_to_client();
+        // erase clients that timed out
+        libserver::server_shared_ptrs.erase(std::remove_if(libserver::server_shared_ptrs.begin(), libserver::server_shared_ptrs.end(), 
+                   boost::bind(&libserver::server::_is_timed_out,std::placeholders::_1)), libserver::server_shared_ptrs.end());
+    }
+}
+
+void libserver::acceptor_thread()
+{
+    boost::asio::ip::tcp::acceptor acceptor(libserver::io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8001));
+    while ( true) 
+    {
+        libserver::server_ptr _new_(new libserver::server);
+        acceptor.accept(_new_->_get_socket());
+        server_shared_ptrs.push_back(_new_);
+    }
 }
